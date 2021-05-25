af0da398cdaf94ef89dab6a84f50201de353f21e
Śeebor Rudnicki <sciborrudnicki@wvvw.dev>
2021-05-25 09:40:28 +0200

    chore: remove unnecessary

--- packages/component-loader/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of component-loader
- */
-
-export * from './lib/component-loader.service';
-export * from './lib/component-loader.component';
-export * from './lib/component-loader.module';
